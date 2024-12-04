d65a0f86e556761cc285d15e7e2ee9c685b9ce20
Sagar Karki <sagarkarki995@gmail.com>
2024-12-04 19:23:53 +0545

    Perform testing with external library for formatCurrency

---
+++ tests-jasmine/testPrice.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../scripts/utils/price.js';
+
+describe('test suite: formatCurrency', () => {
+  it('converts cents into dollars', () => {
+    expect(formatCurrency(2099)).toEqual('20.99');
+  });
+
+  it('zero value input', () => {
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+
+  it('rounding to nearest', () => {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+});
