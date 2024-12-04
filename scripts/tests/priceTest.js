917c8547e8c76db1289800328219621b32d79c5f
Sagar Karki <sagarkarki995@gmail.com>
2024-12-04 08:39:18 +0545

    Automated testing with format currency

---
+++ scripts/tests/priceTest.js
@@ -0,0 +1,27 @@
+import {formatCurrency} from '../utils/price.js';
+
+console.log('test suite: formatCurrency');
+
+console.log('testing with normal digits');
+
+if (formatCurrency(2099) === '20.99') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('with zero input');
+
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('with rounding the numbers');
+
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
